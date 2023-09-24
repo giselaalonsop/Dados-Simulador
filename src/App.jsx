528349310bca644213e80f0c08021e144e69f736
giselaalonsop <giselaalonsop@gmail.com>
2023-09-24 11:58:39 -0400

    Metodos cuantitativos

--- src/App.jsx
+++ src/App.jsx
@@ -1,140 +1,221 @@
-import { useState, useEffect } from "react";
-import {
-  FaDiceOne,
-  FaDiceTwo,
-  FaDiceThree,
-  FaDiceFour,
-  FaDiceFive,
-  FaDiceSix,
-} from "react-icons/fa";
-import "./App.css"; // Importa un archivo CSS personalizado
+import { useState } from "react";
 
-const App = () => {
-  const [dice1, setDice1] = useState(1);
-  const [dice2, setDice2] = useState(1);
-  const [sum, setSum] = useState(2);
-  const [results, setResults] = useState([]);
-  const [totalSum, setTotalSum] = useState(0);
+const profesores = ["A", "B", "C", "D", "E"];
+const cursos = ["C1", "C2", "C3", "C4", "C5"];
 
-  const calculateProbability = (combination) => {
-    const [result1, result2] = combination.split("-").map(Number);
-    return (1 / 36) * (result1 === result2 ? 1 : 2);
-  };
+function App() {
+    const [preferencias, setPreferencias] = useState({
+        C1: [5, 8, 5, 9, 7],
+        C2: [7, 2, 3, 6, 8],
+        C3: [9, 10, 8, 9, 8],
+        C4: [8, 7, 9, 7, 8],
+        C5: [6, 9, 9, 10, 5],
+    });
 
-  const rollDice = () => {
-    // Generar números aleatorios para los dados
-    const result1 = Math.floor(Math.random() * 6) + 1;
-    const result2 = Math.floor(Math.random() * 6) + 1;
+    const [edicionPreferencias, setEdicionPreferencias] = useState(false);
+    const [asignacionOptima, setAsignacionOptima] = useState(null);
 
-    const combination = `${result1}-${result2}`;
-    const currentProbability = calculateProbability(combination);
+    const cambiarPreferencia = (curso, profesor, valor) => {
+        const nuevasPreferencias = { ...preferencias };
+        nuevasPreferencias[curso][profesores.indexOf(profesor)] =
+            parseInt(valor);
+        setPreferencias(nuevasPreferencias);
+    };
 
-    const newResults = [
-      ...results,
-      { combination, probability: currentProbability },
-    ];
-    setResults(newResults);
+    const resolverAsignacion = () => {
+        // Realizar una búsqueda exhaustiva para encontrar la asignación óptima
 
-    // Simula una animación de lanzamiento agregando y eliminando una clase
-    setDice1(0);
-    setDice2(0);
+        const asignacionOptima = {};
+        const cursosAsignados = new Set();
+        const profesoresAsignados = new Set();
 
-    setTimeout(() => {
-      setDice1(result1);
-      setDice2(result2);
+        // Función recursiva para explorar todas las posibles asignaciones
+        const explorarAsignaciones = (cursoIndex) => {
+            if (cursoIndex === cursos.length) {
+                // Si hemos asignado todos los cursos, calculamos la puntuación total
+                let puntuacionTotal = 0;
+                cursos.forEach((curso) => {
+                    const profesorAsignado = asignacionOptima[curso];
+                    const puntuacion =
+                        preferencias[curso][
+                            profesores.indexOf(profesorAsignado)
+                        ];
+                    puntuacionTotal += puntuacion;
+                });
 
-      const currentSum = result1 + result2;
-      setSum(currentSum);
+                // Si es mejor que la asignación actual, la reemplazamos
+                if (
+                    !asignacionOptima.puntuacion ||
+                    puntuacionTotal > asignacionOptima.puntuacion
+                ) {
+                    asignacionOptima.asignacion = { ...asignacionOptima };
+                    asignacionOptima.puntuacion = puntuacionTotal;
+                }
 
-      setTotalSum((prevTotalSum) => prevTotalSum + currentSum);
-    }, 200); // Espera 1 segundo antes de mostrar el resultado
-  };
+                return;
+            }
 
-  useEffect(() => {
-    // Simula la animación de lanzamiento agregando y eliminando la clase 'rolling'
-    if (dice1 === 0 || dice2 === 0) {
-      setTimeout(() => {
-        setDice1(dice1);
-        setDice2(dice2);
-      }, 10);
-    }
-  }, [dice1, dice2]);
+            const cursoActual = cursos[cursoIndex];
+            for (const profesor of profesores) {
+                if (
+                    !cursosAsignados.has(cursoActual) &&
+                    !profesoresAsignados.has(profesor)
+                ) {
+                    // Intentamos asignar el curso al profesor actual
+                    asignacionOptima[cursoActual] = profesor;
+                    cursosAsignados.add(cursoActual);
+                    profesoresAsignados.add(profesor);
 
-  return (
-    <div className="text-center mt-8">
-      <h1 className="text-3xl font-semibold mb-4">
-        Simulación de Lanzamiento de Dados
-      </h1>
-      <div className="flex justify-center items-center space-x-8">
-        <div
-          className={`w-32 h-32 bg-white border border-gray-300 rounded-md flex justify-center items-center text-6xl dice ${
-            dice1 === 1 && "dice-one"
-          } ${dice1 === 2 && "dice-two"} ${dice1 === 3 && "dice-three"} ${
-            dice1 === 4 && "dice-four"
-          } ${dice1 === 5 && "dice-five"} ${dice1 === 6 && "dice-six"}`}
-        >
-          {dice1 === 1 && <FaDiceOne />}
-          {dice1 === 2 && <FaDiceTwo />}
-          {dice1 === 3 && <FaDiceThree />}
-          {dice1 === 4 && <FaDiceFour />}
-          {dice1 === 5 && <FaDiceFive />}
-          {dice1 === 6 && <FaDiceSix />}
+                    // Pasamos al siguiente curso
+                    explorarAsignaciones(cursoIndex + 1);
+
+                    // Deshacemos la asignación para explorar otras opciones
+                    delete asignacionOptima[cursoActual];
+                    cursosAsignados.delete(cursoActual);
+                    profesoresAsignados.delete(profesor);
+                }
+            }
+        };
+
+        // Iniciamos la búsqueda exhaustiva desde el primer curso
+        explorarAsignaciones(0);
+
+        // Actualizar el estado con la asignación óptima encontrada
+        setAsignacionOptima(asignacionOptima);
+    };
+
+    return (
+        <div className="container mx-auto mt-5 p-5 content-center">
+            <h1 className="text-lg font-bold mb-3 text-center ">
+                Asignación de Profesores a Cursos
+            </h1>
+            <div className="w-full max-w-xl mx-auto">
+                <div className="mb-5 ">
+                    <table className="w-full border-collapse border sm:rounded-lg">
+                        <thead className="text-xs text-gray-700 uppercase bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
+                            <tr>
+                                <th scope="col" className="px-6 py-3">
+                                    Curso
+                                </th>
+                                {profesores.map((profesor, index) => (
+                                    <th
+                                        key={index}
+                                        scope="col"
+                                        className="px-6 py-3"
+                                    >
+                                        Profesor {profesor}
+                                    </th>
+                                ))}
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {cursos.map((curso, cursoIndex) => (
+                                <tr
+                                    key={cursoIndex}
+                                    className="bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-600"
+                                >
+                                    <th
+                                        scope="row"
+                                        className="px-6 py-4 font-semibold text-gray-900 dark:text-white"
+                                    >
+                                        Curso {curso}
+                                    </th>
+                                    {profesores.map(
+                                        (profesor, profesorIndex) => (
+                                            <td
+                                                key={profesorIndex}
+                                                className="px-6 py-4  text-white"
+                                            >
+                                                {edicionPreferencias ? (
+                                                    <input
+                                                        type="number"
+                                                        className="w-10 h-8 text-center border rounded-lg text-black"
+                                                        value={
+                                                            preferencias[curso][
+                                                                profesorIndex
+                                                            ]
+                                                        }
+                                                        onChange={(e) =>
+                                                            cambiarPreferencia(
+                                                                curso,
+                                                                profesor,
+                                                                e.target.value
+                                                            )
+                                                        }
+                                                    />
+                                                ) : (
+                                                    preferencias[curso][
+                                                        profesorIndex
+                                                    ]
+                                                )}
+                                            </td>
+                                        )
+                                    )}
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+                <div className="flex justify-center mb-5">
+                    <button
+                        onClick={() =>
+                            setEdicionPreferencias(!edicionPreferencias)
+                        }
+                        className="px-4 py-2 mr-3 bg-blue-500 text-white rounded hover:bg-blue-700"
+                    >
+                        {edicionPreferencias
+                            ? "Guardar Preferencias"
+                            : "Editar Preferencias"}
+                    </button>
+                    <button
+                        onClick={resolverAsignacion}
+                        className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-700"
+                    >
+                        Resolver Asignación
+                    </button>
+                </div>
+                {asignacionOptima && (
+                    <div>
+                        <h2 className="text-lg font-bold mb-3 text-center">
+                            Asignación Óptima
+                        </h2>
+                        <table className="w-full border-collapse border rounded-lg">
+                            <thead className="text-xs text-gray-700 uppercase bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
+                                <tr>
+                                    <th scope="col" className="px-6 py-3">
+                                        Curso
+                                    </th>
+                                    <th scope="col" className="px-6 py-3">
+                                        Profesor Asignado
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {cursos.map((curso, cursoIndex) => (
+                                    <tr
+                                        key={cursoIndex}
+                                        className="bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-600"
+                                    >
+                                        <th
+                                            scope="row"
+                                            className="px-6 py-4 font-semibold text-gray-900 dark:text-white "
+                                        >
+                                            Curso {curso}
+                                        </th>
+                                        <td className="px-6 py-4 align-top text-center text-white">
+                                            {" "}
+                                            {/* Agregamos align-top aquí */}
+                                            {asignacionOptima.asignacion[curso]}
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                )}
+            </div>
         </div>
-        <div
-          className={`w-32 h-32 bg-white border border-gray-300 rounded-md flex justify-center items-center text-6xl dice ${
-            dice2 === 1 && "dice-one"
-          } ${dice2 === 2 && "dice-two"} ${dice2 === 3 && "dice-three"} ${
-            dice2 === 4 && "dice-four"
-          } ${dice2 === 5 && "dice-five"} ${dice2 === 6 && "dice-six"}`}
-        >
-          {dice2 === 1 && <FaDiceOne />}
-          {dice2 === 2 && <FaDiceTwo />}
-          {dice2 === 3 && <FaDiceThree />}
-          {dice2 === 4 && <FaDiceFour />}
-          {dice2 === 5 && <FaDiceFive />}
-          {dice2 === 6 && <FaDiceSix />}
-        </div>
-      </div>
-      <p className="text-2xl mt-4">Suma: {sum}</p>
-      <button
-        className="mt-8 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
-        onClick={rollDice}
-        disabled={dice1 === 0 || dice2 === 0}
-      >
-        Lanzar Dados
-      </button>
-      <div className="mt-8">
-        <h2 className="text-3xl font-semibold mb-4">Suma Total:</h2>
-        <p className="text-4xl">{totalSum}</p>
-      </div>
-      <div className="mt-8">
-        <h2 className="text-3xl font-semibold mb-4">
-          Probabilidades de Combinaciones:
-        </h2>
-        <table className="min-w-full border border-gray-300">
-          <thead>
-            <tr>
-              <th className="border border-gray-300 p-2">Combinación</th>
-              <th className="border border-gray-300 p-2">Probabilidad</th>
-            </tr>
-          </thead>
-          <tbody>
-            {results.map((result, index) => (
-              <tr key={index}>
-                <td className="border border-gray-300 p-2">
-                  {result.combination}
-                </td>
-                <td className="border border-gray-300 p-2">
-                  {result.probability.toFixed(4)} (
-                  {(result.probability * 100).toFixed(2)}%)
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </div>
-  );
-};
+    );
+}
 
 export default App;
